56e80e055824619da612b4b96c4fa4a0b7575e45
Anubhav <anubhav.choudhary@live.com>
2015-12-28 22:08:48 +0530

    v1.0

---
+++ Prob1/app.js
@@ -0,0 +1,12 @@
+// Paging Problem
+window.onload = function() {
+	// Your code here
+}
+
+function prevPage() {
+	// Your code here
+}
+
+function nextPage() {
+	// Your code here
+}
---
+++ Prob2/app.js
@@ -0,0 +1,11 @@
+// Paging Problem
+window.onload = function() {
+	// Inital Form Data
+	var name = "Anubhav Choudhary";
+	var dateofbirth = '08/02/1993';
+	var gender = 'M';
+	var country = 'SL'
+
+	// Your code here to set given data to form
+}
+
