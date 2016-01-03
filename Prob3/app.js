2f6eb2e5ec10044ce90e09becc5380c1c656c31a
Anubhav <anubhav.choudhary@live.com>
2016-01-03 19:02:47 +0530

    Prob3 and Prob4 added.

---
+++ Prob3/app.js
@@ -0,0 +1,18 @@
+// Image Slider Problem
+window.onload = function() {
+	// slidetime, height, width
+	var slidetime = 1000; //milliseconds
+	var height = 300; //px
+	var width = 600; //px
+
+	// calling initSlider
+	initSlider(slidetime, height, width);
+}
+
+function initSlider(slidetime, height, width)
+{
+	//your code here
+
+}
+
+//your code here if required
---
+++ Prob4/app.js
@@ -0,0 +1,8 @@
+// Read More Problem
+window.onload = function() {
+	// you code here
+}
+
+// you code here if required
+
+
